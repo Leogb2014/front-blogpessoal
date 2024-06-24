f13a7531cd04326099f769282b71ee8d50a8b82d
Leonardo Gobetti <leonardogobetti@hotmail.com>
2024-06-24 13:45:40 -0300

    Props e Hooks testes feitos

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,22 @@
+import { useState } from 'react'
 import './App.css'
 import Home from './paginas/home/Home'
 
 
 function App() {
+  const[valor, setValor] = useState(0);
+
+  function handleClick(){
+    setValor(valor + 1);
+  }
   return (
     <>
-    <Home/>
+    <div>
+      <p>O valor é: {valor}</p>
+      <button onClick={handleClick}>Adicionar 1 </button>
+    </div>
+    <Home title='Home'
+          description='Este é um compponente Home que recebe props'/>
     </>
   )
 }
--- src/paginas/home/Home.tsx
+++ src/paginas/home/Home.tsx
@@ -1,13 +1,40 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import './Home.css'
 import homeLogo from '../../assets/home.png'
 
-const Home = () => {
+interface myProps {
+  title: string,
+  description: string
+}
+
+function Home(props:myProps) {
+  const[completed, setCompleted] = useState(false);
+  const[tarefa, setTarefa] = useState("");
+  const[logar, setLogar] = useState(false);
+
+  useEffect(() => {
+    if(completed) {
+      setTarefa('Parabéns! Você concluiu a tarefa!');
+    }
+  }, [completed])
   return (
     <>
-      <h1 className='titulo'>Home</h1>
+    <div>
+      <h2>Tarefa</h2>
+      <h3>{tarefa}</h3>
+      <p>Conclua a tarefa</p>
+      <button onClick={() => setCompleted(true)}>Concluir tarefa</button>
+    </div>
+      <h1 className='titulo'> {props.title}</h1>
+      <p>{props.description}</p>
 
       <img src={homeLogo} className='img' alt="" />
+
+      {logar ? (
+        <h2>Bem vindo de volta!</h2>) : (
+          <button onClick={() => setLogar(true)}>entrar</button>
+        )
+      }
     </>
   
   )
